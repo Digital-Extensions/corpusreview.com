9d30e375b9c9df1e877211bea4b3d56cb23c56d1
Kent Merrell <kmerrell@gmail.com>
2026-02-09 14:12:52 +0000

    Update latest version to 1.2.0 in Download component

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -10,7 +10,7 @@ import Footer from "@/components/Footer";
 
 const GITHUB_RELEASES_URL =
   "https://github.com/Digital-Extensions/corpus-review-releases/releases";
-const LATEST_VERSION = "1.1.9";
+const LATEST_VERSION = "1.2.0";
 
 const downloads = {
   mac: {
